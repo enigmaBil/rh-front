393c6c406f17e380ac1af60276e4f5f3330acaf0
Bilong Emmanuel <emmanueldigital9@gmail.com>
2025-03-01 17:59:58 +0100

    Initiate front end app

---
+++ src/app/layouts/sidebar-right/sidebar-right.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-right',
+  imports: [],
+  templateUrl: './sidebar-right.component.html',
+  styleUrl: './sidebar-right.component.css'
+})
+export class SidebarRightComponent {
+
+}
---
+++ src/app/layouts/sidebar/sidebar.component.ts
@@ -0,0 +1,51 @@
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+  // menuItems = routesAdmin
+  currentTheme = "";
+  activeItem: string | null = null;
+  openSubmenus: { [key: string]: boolean } = {};
+
+  constructor(
+    @Inject(DOCUMENT) private doc: Document,
+  ) {
+  }
+
+  ngOnInit(): void {
+    const theme = localStorage.getItem("theme");
+    if (theme && ["dark", "light"].includes(theme)) {
+      this.currentTheme = theme;
+      document.documentElement.firstElementChild?.classList?.toggle(theme, true);
+    }
+
+  }
+
+  toggleTheme() {
+    console.log(this.currentTheme);
+
+    if (this.currentTheme === "dark") {
+      this.doc.documentElement.classList.remove("dark");
+      this.currentTheme = "light";
+      localStorage.setItem("theme", "light");
+    } else {
+      this.doc.documentElement.classList?.add("dark");
+      this.currentTheme = "dark";
+      localStorage.setItem("theme", "dark");
+    }
+  }
+
+  setActiveItem(path: string) {
+    this.activeItem = path;
+  }
+
+  toggleSubmenu(itemName: string) {
+    this.openSubmenus[itemName] = !this.openSubmenus[itemName];
+  }
+}
